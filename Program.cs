29af6bf6b9ee8502d13d3973df1429a3b111c9cc
Narin1051 <narin.phue@mail.kmutt.ac.th>
2023-03-13 14:46:21 +0700

    input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program{
+    static void Main (string[] args){
+
+        Console.WriteLine ("Plz input first paper center X : ");
+        int x1 = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Plz input first paper center Y : ");
+        int y1 = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Plz input first paper wide : ");
+        int wide1 = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Plz input first paper hieght : ");
+        int height1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine ("Plz input second paper center X : ");
+        int x2 = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Plz input second paper center Y : ");
+        int y2 = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Plz input second paper wide : ");
+        int wide2 = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Plz input second paper hieght : ");
+        int height2 = int.Parse(Console.ReadLine());
+
+        
+
+    }
+}
